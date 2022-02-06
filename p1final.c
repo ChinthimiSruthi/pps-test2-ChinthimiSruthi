fc4d322c6d63cd45633744ff1be7ff3df837f9ec
Sruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2022-02-06 07:20:37 +0000

    p1 final

--- p1final.c
+++ p1final.c
@@ -3,7 +3,7 @@
 int main()
 {
   float x1,y1,x2,y2,d;
-  printf("Enter the coordinates of point 1\n");      scanf("%f%f",&x1,&y1)
+  printf("Enter the coordinates of point 1\n");      scanf("%f%f",&x1,&y1);
   printf("Enter the coordinates of point 2\n");
   scanf("%f%f",&x2,&y2);
   d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
